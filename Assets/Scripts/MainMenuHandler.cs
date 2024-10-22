f516dc7edce8596db68296684d596aa3ebb2de4c
Zubek <imkekx@gmail.com>
2024-10-22 22:18:04 +0300

    Addin name input validation

---
+++ Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class MainMenuHandler : MonoBehaviour
+{
+
+    public TMP_InputField inputNameText;
+    public TMP_Text validationMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(inputNameText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TextInputValidation() 
+    {
+        string playerName = inputNameText.text;
+        if (playerName == "")
+        {
+            validationMessage.gameObject.SetActive(true);
+        }
+        else
+        {
+            validationMessage.gameObject.SetActive(false);
+        }
+    }
+
+    public void DebugInput() 
+    {
+        string playerName = inputNameText.text;
+        
+        if(playerName == "") 
+        {
+            validationMessage.gameObject.SetActive(true);
+            Debug.Log("Required enter name");
+        }
+        else 
+        { 
+            Debug.Log(inputNameText.text);        
+        }
+    }
+}
